2c5d824f1e4c4e5acf575b029d5d3daba943e3eb
B-RyeTheAsianSpy <33035218+B-RyeTheAsianSpy@users.noreply.github.com>
2019-02-07 17:15:10 -0800

    Update Project1.sql

--- CECS 323 Java Project/src/cecs/pkg323/java/project/Project1.sql
+++ CECS 323 Java Project/src/cecs/pkg323/java/project/Project1.sql
@@ -1,43 +1,93 @@
--- BY BRIAN NGUYEN
-
--- TABLES --
-
--- WritingGroup table
--- PK: GroupName
 create table WritingGroup(
-
+  GroupName varchar(30) PRIMARY KEY, 
+  HeadWriter varchar(15), 
+  YearFormed int, 
+  Subject varchar(20)
 );
-
--- Publishers talbe
--- PK: PublisherName
 create table Publishers(
-
+  PublisherName varchar(30) PRIMARY KEY, 
+  PublisherAddress varchar(50), 
+  PublisherPhone varchar(12), 
+  PublisherEmail varchar(50)
 );
-
--- Books table
--- the association class of WritingGroup and Publishers
--- PK: GroupName, BookTitle
--- FK: GroupName, PublisherName
 create table Books(
-
+  -- the association class
+  GroupName varchar(30), 
+  BookTitle varchar(30), 
+  PublisherName varchar(20) NOT NULL, 
+  YearPublished int, 
+  NumberOfPages int, 
+  FOREIGN KEY (GroupName) REFERENCES WritingGroup(GroupName), 
+  FOREIGN KEY (PublisherName) REFERENCES Publishers(PublisherName)
 );
-
-ALTER TABLE Books
-
-;
-ALTER TABLE Books
-;
-
--- VALUES --
-
--- WrtingGroup
-INSERT INTO WritingGroup(...)
-  
-
-  
--- Publishers
-INSERT INTO Publishers(...);
-
--- Books
-INSERT INTO Books(...);
-
+INSERT INTO WritingGroup(
+  GroupName, HeadWriter, YearFormed, 
+  Subject
+) 
+VALUES 
+  (
+    'Benji Book Club', 'Benji', 1998, 
+    'Sci-Fi'
+  );
+INSERT INTO WritingGroup(
+  GroupName, HeadWriter, YearFormed, 
+  Subject
+) 
+VALUES 
+  (
+    'Jing Si Abode', 'Cheng Yen', 1960, 
+    'Aphorisms'
+  );
+INSERT INTO WritingGroup(
+  GroupName, HeadWriter, YearFormed, 
+  Subject
+) 
+VALUES 
+  (
+    'Lego Group', 'Alf', 1998, 'Lego Bricks'
+  );
+INSERT INTO WritingGroup(
+  GroupName, HeadWriter, YearFormed, 
+  Subject
+) 
+VALUES 
+  (
+    'Upcoming Writers', 'Chelsea', 2018, 
+    'Journalism'
+  );
+INSERT INTO Publishers(
+  PublisherName, PublisherAddress, 
+  PublisherPhone, PublisherEmail
+) 
+VALUES 
+  (
+    '49ers Publishing', '562 Bellflower St.', 
+    '562-421-4219', '49publishing@gmail.com'
+  );
+INSERT INTO Publishers(
+  PublisherName, PublisherAddress, 
+  PublisherPhone, PublisherEmail
+) 
+VALUES 
+  (
+    'Jing Si Publishing', ' 7821 Hualien St.', 
+    '822-810-0481', 'jingsi@gmail.com'
+  );
+INSERT INTO Books(
+  GroupName, BookTitle, PublisherName, 
+  YearPublished, NumberOfPages
+) 
+VALUES 
+  (
+    'Benji Book Club', 'The Biography of Benji', 
+    '49ers Publishing', 1998, 341
+  );
+INSERT INTO Books(
+  GroupName, BookTitle, PublisherName, 
+  YearPublished, NumberOfPages
+) 
+VALUES 
+  (
+    'Upcoming Writers', 'History CSULB', 
+    '49ers Publishing', 2020, 666
+  );
